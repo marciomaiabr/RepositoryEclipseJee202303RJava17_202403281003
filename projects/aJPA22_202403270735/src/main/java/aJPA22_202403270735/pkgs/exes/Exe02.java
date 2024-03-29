0bc33ccb3c28105e54d08924d12bcc69a9560f71
marciomaiabr <marcio.luis.maia@gmail.com>
2024-03-29 03:28:06 -0400

    adding aModelsJPA22_202403290320 project

---
+++ projects/aJPA22_202403270735/src/main/java/aJPA22_202403270735/pkgs/exes/Exe02.java
@@ -0,0 +1,51 @@
+/*
+teste de criação de entidades no banco
+*/
+package aJPA22_202403270735.pkgs.exes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import aJPA22_202403270735.pkgs.utils.JPAUtil;
+
+public class Exe02 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		EntityTransaction et = null;
+
+		try {
+			emf = JPAUtil.criaEntityManagerFactoryWithCreateCreate();
+			System.out.println("[emf=" + (emf) + "]");
+
+			em = emf.createEntityManager();
+			System.out.println("[em=" + (em) + "]");
+
+			et = em.getTransaction();
+			System.out.println("[et=" + (et) + "]");
+
+			et.begin();
+
+			et.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				et.rollback();
+			} catch (Exception e2) {
+			}
+		} finally {
+			try {
+				em.close();
+			} catch (Exception e2) {
+			}
+			try {
+				emf.close();
+			} catch (Exception e2) {
+			}
+		}
+
+	}
+
+}
---
+++ projects/aModelsJPA22_202403290320/src/main/java/aModelsJPA22_202403290320/pkgs/models/Pessoa.java
@@ -0,0 +1,65 @@
+package aModelsJPA22_202403290320.pkgs.models;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+
+	public Pessoa() {
+		super();
+	}
+
+	public Pessoa(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+}

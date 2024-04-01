fd2f93a66c2611ceb06b0dbf4d7c2093f5c6024f
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-01 16:49:24 -0400

    adding implementation for PessoasServiceSpec

---
+++ projects/aServicesImpl202404011548/src/main/java/aServicesImpl202404011548/pkgs/services/implementations/PessoasServiceImpl.java
@@ -0,0 +1,41 @@
+package aServicesImpl202404011548.pkgs.services.implementations;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import aModelsJPA22_202403290320.pkgs.models.Pessoa;
+import aRepositoriesSpec202404010931.pkgs.repositories.specifications.PessoasSpec;
+import aServicesSpec202404011523.pkgs.services.specifications.PessoasServiceSpec;
+
+public class PessoasServiceImpl implements PessoasServiceSpec {
+
+	@Inject
+	private PessoasSpec pessoas;
+
+	@Override
+	public Pessoa salvar(Pessoa pessoa) {
+		return pessoas.save(pessoa);
+	}
+
+	@Override
+	public Pessoa buscar(Integer id) {
+		return pessoas.get(id);
+	}
+
+	@Override
+	public List<Pessoa> listar() {
+		return pessoas.list();
+	}
+
+	@Override
+	public List<Pessoa> listarPorNome(String nome) {
+		return pessoas.listByNome(nome);
+	}
+
+	@Override
+	public void excluir(Pessoa pessoa) {
+		pessoas.delete(pessoa);
+	}
+
+}

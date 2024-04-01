86b8ec1ce2a317e094599b205b494ab1182a806b
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-01 15:13:50 -0400

    testing Pessoas Repositories Inject

--- projects/aJSF22CDI20_202403280300/src/main/java/aJSF22CDI20_202403280300/pkgs/controllers/Teste01Controller.java
+++ projects/aJSF22CDI20_202403280300/src/main/java/aJSF22CDI20_202403280300/pkgs/controllers/Teste01Controller.java
@@ -5,19 +5,19 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 
-import aJPA22_202403270735.pkgs.utils.JPAUtil;
-import aModelsJPA22_202403290320.pkgs.models.Pessoa;
+import aRepositoriesSpec202404010931.pkgs.repositories.specifications.PessoasSpec;
 
 @Named
 @ViewScoped
 public class Teste01Controller implements Serializable {
 
 	private static final long serialVersionUID = 20240328032700L;
+
+	@Inject
+	private PessoasSpec pessoas;
 
 	static {
 		System.out.println("Teste01Controller.static");
@@ -37,46 +37,8 @@ public class Teste01Controller implements Serializable {
 	}
 
 	public void method01() {
-		System.out.println("Teste01Controller.method01()[" + (this) + "]");
-
-		EntityManagerFactory emf = null;
-		EntityManager em = null;
-		EntityTransaction et = null;
-
-		try {
-			emf = JPAUtil.criaEntityManagerFactoryWithFromPersistenceUnit();
-			System.out.println("[emf=" + (emf) + "]");
-
-			em = emf.createEntityManager();
-			System.out.println("[em=" + (em) + "]");
-
-			et = em.getTransaction();
-			System.out.println("[et=" + (et) + "]");
-
-			et.begin();
-
-			Pessoa pessoa = new Pessoa("Fulano", "Silva");
-			em.merge(pessoa);
-
-			et.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			try {
-				et.rollback();
-			} catch (Exception e2) {
-			}
-		} finally {
-			try {
-				em.close();
-			} catch (Exception e2) {
-			}
-			try {
-				emf.close();
-			} catch (Exception e2) {
-			}
-		}
-
-		System.out.println("[" + (JPAUtil.criaEntityManagerFactoryWithFromPersistenceUnit()) + "]");
+		System.out.println("Teste01Controller.method01()[" + (this) + "][pessoas=" + (pessoas) + "]");
+		pessoas.list().forEach(System.out::println);
 	}
 
 	@PreDestroy

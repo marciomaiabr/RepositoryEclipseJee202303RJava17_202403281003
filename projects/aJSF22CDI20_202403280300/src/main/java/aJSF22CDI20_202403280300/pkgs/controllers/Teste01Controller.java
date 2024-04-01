1d9054cd02b70c6d701f35f858cd431991e74800
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-01 09:00:28 -0400

    testing access and persist with new JPA configuration

--- projects/aJSF22CDI20_202403280300/src/main/java/aJSF22CDI20_202403280300/pkgs/controllers/Teste01Controller.java
+++ projects/aJSF22CDI20_202403280300/src/main/java/aJSF22CDI20_202403280300/pkgs/controllers/Teste01Controller.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 import aJPA22_202403270735.pkgs.utils.JPAUtil;
 import aModelsJPA22_202403290320.pkgs.models.Pessoa;
-
-import javax.faces.view.ViewScoped;
 
 @Named
 @ViewScoped
@@ -37,8 +39,42 @@ public class Teste01Controller implements Serializable {
 	public void method01() {
 		System.out.println("Teste01Controller.method01()[" + (this) + "]");
 
-		Pessoa pessoa = null;
-		System.out.println("[pessoa=" + (pessoa) + "]");
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		EntityTransaction et = null;
+
+		try {
+			emf = JPAUtil.criaEntityManagerFactoryWithFromPersistenceUnit();
+			System.out.println("[emf=" + (emf) + "]");
+
+			em = emf.createEntityManager();
+			System.out.println("[em=" + (em) + "]");
+
+			et = em.getTransaction();
+			System.out.println("[et=" + (et) + "]");
+
+			et.begin();
+
+			Pessoa pessoa = new Pessoa("Fulano");
+			em.merge(pessoa);
+
+			et.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				et.rollback();
+			} catch (Exception e2) {
+			}
+		} finally {
+			try {
+				em.close();
+			} catch (Exception e2) {
+			}
+			try {
+				emf.close();
+			} catch (Exception e2) {
+			}
+		}
 
 		System.out.println("[" + (JPAUtil.criaEntityManagerFactoryWithFromPersistenceUnit()) + "]");
 	}

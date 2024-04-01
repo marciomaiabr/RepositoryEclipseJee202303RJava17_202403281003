36e48ac1db40e153dad61ee69944d6d11c6d7a74
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-01 15:11:29 -0400

    adding class to create EntityManager

---
+++ projects/aJSF22CDI20_202403280300/src/main/java/aJSF22CDI20_202403280300/pkgs/producers/EntityManagerProducer.java
@@ -0,0 +1,38 @@
+package aJSF22CDI20_202403280300.pkgs.producers;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import aJPA22_202403270735.pkgs.utils.JPAUtil;
+
+@ApplicationScoped
+public class EntityManagerProducer implements Serializable {
+
+	private static final long serialVersionUID = 20240328032700L;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public EntityManagerProducer() {
+		System.out.println("EntityManagerProducer.()");
+		entityManagerFactory = JPAUtil.criaEntityManagerFactory();
+	}
+
+	@Produces
+	@RequestScoped
+	public EntityManager createEntityManager() {
+		System.out.println("EntityManagerProducer.createEntityManager()");
+		return entityManagerFactory.createEntityManager();
+	}
+
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		System.out.println("EntityManagerProducer.closeEntityManager()");
+		manager.close();
+	}
+
+}

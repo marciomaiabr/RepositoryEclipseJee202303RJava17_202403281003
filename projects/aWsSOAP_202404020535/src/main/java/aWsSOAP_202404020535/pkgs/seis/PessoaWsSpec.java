2a3958b23176a2355c74ab17d01c0357fbc97be9
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-02 08:00:02 -0400

    adding specifications and implementations of service

---
+++ projects/aWsSOAP_202404020535/src/main/java/aWsSOAP_202404020535/pkgs/seis/PessoaWsSpec.java
@@ -0,0 +1,28 @@
+package aWsSOAP_202404020535.pkgs.seis;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+import aModelsJPA22_202403290320.pkgs.models.Pessoa;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public interface PessoaWsSpec {
+
+	@WebMethod
+	Pessoa salvar(Pessoa pessoa);
+
+	@WebMethod
+	Pessoa buscar(Integer id);
+
+	@WebMethod
+	List<Pessoa> listar();
+
+	@WebMethod
+	void excluir(Pessoa pessoa);
+
+}
---
+++ projects/aWsSOAP_202404020535/src/main/java/aWsSOAP_202404020535/pkgs/sibs/PessoaWsImpl.java
@@ -0,0 +1,42 @@
+package aWsSOAP_202404020535.pkgs.sibs;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import aModelsJPA22_202403290320.pkgs.models.Pessoa;
+import aServicesSpec202404011523.pkgs.services.specifications.PessoasServiceSpec;
+import aWsSOAP_202404020535.pkgs.seis.PessoaWsSpec;
+
+@WebService(endpointInterface = "aWsSOAP_202404020535.pkgs.seis.PessoaWsSpec")
+public class PessoaWsImpl implements PessoaWsSpec {
+
+	public PessoaWsImpl() {
+		System.out.println("PessoaWsImpl.PessoaWsImpl()");
+	}
+
+	@Inject
+	private PessoasServiceSpec pessoasService;
+
+	@Override
+	public Pessoa salvar(Pessoa pessoa) {
+		return pessoasService.salvar(pessoa);
+	}
+
+	@Override
+	public Pessoa buscar(Integer id) {
+		return pessoasService.buscar(id);
+	}
+
+	@Override
+	public List<Pessoa> listar() {
+		return pessoasService.listar();
+	}
+
+	@Override
+	public void excluir(Pessoa pessoa) {
+		pessoasService.excluir(pessoa);
+	}
+
+}

91fa0cdd0db37090322ecfd436040ff7828588f7
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-05 12:08:48 -0400

    adding aModelsSpecifications_202404051154 project

---
+++ projects/aModelsSpecifications_202404051154/src/main/java/com/mm/models/specifications/Pessoa.java
@@ -0,0 +1,12 @@
+package com.mm.models.specifications;
+
+public interface Pessoa {
+
+	Integer getId();
+	Integer setId(Integer id);
+	String getNome();
+	String setNome(String nome);
+	String getSobreNome();
+	String setSobreNome(String sobreNome);
+
+}

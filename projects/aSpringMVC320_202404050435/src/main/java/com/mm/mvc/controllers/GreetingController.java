3eaed16bb4131c2ecf96337f8cc4115fdf635097
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-05 05:05:38 -0400

    change packages

--- projects/aSpringMVC320_202404050435/src/main/java/com/mm/mvc/controllers/GreetingController.java
+++ projects/aSpringMVC320_202404050435/src/main/java/com/mm/mvc/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package com.mm.servingwebcontent;
+package com.mm.mvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;

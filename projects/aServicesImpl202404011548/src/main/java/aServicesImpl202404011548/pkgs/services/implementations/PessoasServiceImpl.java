6553c7f76abd0f4e3c1a21a23081da027e2eda9b
marciomaiabr <marcio.luis.maia@gmail.com>
2024-04-09 09:22:24 -0400

    using dto

--- projects/aServicesImpl202404011548/src/main/java/aServicesImpl202404011548/pkgs/services/implementations/PessoasServiceImpl.java
+++ projects/aServicesImpl202404011548/src/main/java/aServicesImpl202404011548/pkgs/services/implementations/PessoasServiceImpl.java
@@ -7,39 +7,38 @@ import java.util.List;
 import javax.inject.Inject;
 
 import com.mm.dtos.PessoaDTO;
-import com.mm.models.specifications.IPessoa;
 import com.mm.utils.DTOUtil;
 
 import aServicesSpec202404011523.pkgs.services.specifications.PessoasServiceSpec;
-import pkgs.persistence.repositories.specifications.PessoasSpec;
+import pkgs.persistence.repositories.implementations.PessoasJPA;
 
 public class PessoasServiceImpl implements Serializable, PessoasServiceSpec {
 
 	private static final long serialVersionUID = 20240328032700L;
 
 	@Inject
-	private PessoasSpec<IPessoa> pessoas;
+	private PessoasJPA pessoas;
 
 	@Override
 	public PessoaDTO salvar(PessoaDTO pessoa) {
-		return DTOUtil.convertIPessoaToPessoaDTO(pessoas.save(DTOUtil.convertPessoaDTOToIPessoa(pessoa)));
+		return DTOUtil.convertPessoaJPAToPessoaDTO(pessoas.save(DTOUtil.convertPessoaDTOToPessoaJPA(pessoa)));
 	}
 
 	@Override
 	public PessoaDTO buscar(Integer id) {
-		return DTOUtil.convertIPessoaToPessoaDTO(pessoas.get(id));
+		return DTOUtil.convertPessoaJPAToPessoaDTO(pessoas.get(id));
 	}
 
 	@Override
 	public List<PessoaDTO> listar() {
 		List<PessoaDTO> list = new ArrayList<>();
-		pessoas.list().forEach(iPessoa -> list.add(DTOUtil.convertIPessoaToPessoaDTO(iPessoa)));
+		pessoas.list().forEach(iPessoa -> list.add(DTOUtil.convertPessoaJPAToPessoaDTO(iPessoa)));
 		return list;
 	}
 
 	@Override
 	public void excluir(PessoaDTO pessoa) {
-		pessoas.delete(DTOUtil.convertPessoaDTOToIPessoa(pessoa));
+		pessoas.delete(DTOUtil.convertPessoaDTOToPessoaJPA(pessoa));
 	}
 
 }
